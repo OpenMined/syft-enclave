50a9646264b778a81c85f4f49128a4441b434e21
rasswanth s <43314053+rasswanth-s@users.noreply.github.com>
2023-03-14 09:32:59 +0530

    lint

--- src/dev_uvicorn.py
+++ src/dev_uvicorn.py
@@ -1,10 +1,10 @@
+import sys
+
 import uvicorn
 
 from app import app
-import sys
 
-
-port = int(sys.argv[1]) if len(sys.argv)==2 else 3030
+port = int(sys.argv[1]) if len(sys.argv) == 2 else 3030
 
 
 if __name__ == "__main__":
